87565839488bcfb26456f153fd061b93d1a38755
Md Ishtiyaque <webdesigner1084@gmail.com>
2026-06-16 13:00:44 +0530

    xyz

--- projects/shell/webpack.config.js
+++ projects/shell/webpack.config.js
@@ -3,8 +3,10 @@ const { shareAll, withModuleFederationPlugin } = require('@angular-architects/mo
 module.exports = withModuleFederationPlugin({
 
   remotes: {
-    "product": "http://localhost:4201/remoteEntry.js",
-    "account": "http://localhost:4202/remoteEntry.js",
+    // "product": "http://localhost:4201/remoteEntry.js",
+    // "account": "http://localhost:4202/remoteEntry.js",
+    "product": 'https://product-app.netlify.app/remoteEntry.js',
+    "account": 'https://account-app.netlify.app/remoteEntry.js'
   },
 
   shared: {
